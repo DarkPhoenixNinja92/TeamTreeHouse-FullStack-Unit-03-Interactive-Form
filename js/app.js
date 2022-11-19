9591cbbcedb5930c801408168558108707956f04
Shaun <scpour@gmail.com>
2022-11-18 23:26:44 -0500

    added color and design variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const nameInput = document.getElementById('name');
 const jobRole = document.querySelector('select[name="user-title"]')
 const otherRole = document.querySelector('input[name="other-job-role"]');
+const shirtDesign = document.getElementById('design');
+const shirtColor = document.getElementById('color');
+const colorOptions = document.querySelectorAll('#color option');
 
 nameInput.focus();
 otherRole.style.opacity = '0';
